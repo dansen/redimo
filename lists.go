4caf37f3232339ba7a5c2575e371947da27f2675
dansen <370283593@qq.com>
2024-04-29 16:10:30 +0800

    Update lists.go

--- lists.go
+++ lists.go
@@ -8,6 +8,13 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
 
+type LSide string
+
+const (
+	Left  LSide = "LEFT"
+	Right LSide = "RIGHT"
+)
+
 func (c Client) LINDEX(key string, index int64) (element ReturnValue, err error) {
 	return ReturnValue{}, err
 }
@@ -30,14 +37,14 @@ func (c Client) createRightIndex(key string) (index float64, err error) {
 	return float64(v), err
 }
 
-func (c Client) LPUSH(key string, vElements ...string) (newLength int64, err error) {
+func (c Client) LPUSH(key string, vElements ...interface{}) (newLength int64, err error) {
 	length, err := c.LLEN(key)
 
 	if err != nil {
 		return length, err
 	}
 
-	for index, member := range vElements {
+	for index, e := range vElements {
 		builder := newExpresionBuilder()
 
 		score, err := c.createLeftIndex(key)
@@ -48,6 +55,7 @@ func (c Client) LPUSH(key string, vElements ...string) (newLength int64, err err
 
 		// snk 是分数
 		builder.updateSetAV(c.sortKeyNum, zScore{score}.ToAV())
+		member := e.(string)
 
 		_, err = c.ddbClient.UpdateItem(context.TODO(), &dynamodb.UpdateItemInput{
 			ConditionExpression:       builder.conditionExpression(),
@@ -71,14 +79,14 @@ func (c Client) LPUSH(key string, vElements ...string) (newLength int64, err err
 	return length + int64(len(vElements)), nil
 }
 
-func (c Client) RPUSH(key string, vElements ...string) (newLength int64, err error) {
+func (c Client) RPUSH(key string, vElements ...interface{}) (newLength int64, err error) {
 	length, err := c.LLEN(key)
 
 	if err != nil {
 		return length, err
 	}
 
-	for index, member := range vElements {
+	for index, e := range vElements {
 		builder := newExpresionBuilder()
 
 		score, err := c.createRightIndex(key)
@@ -89,6 +97,7 @@ func (c Client) RPUSH(key string, vElements ...string) (newLength int64, err err
 
 		// snk 是分数
 		builder.updateSetAV(c.sortKeyNum, zScore{score}.ToAV())
+		member := e.(string)
 
 		_, err = c.ddbClient.UpdateItem(context.TODO(), &dynamodb.UpdateItemInput{
 			ConditionExpression:       builder.conditionExpression(),
@@ -108,4 +117,35 @@ func (c Client) RPUSH(key string, vElements ...string) (newLength int64, err err
 			return length + int64(index), err
 		}
 	}
+
+	return length + int64(len(vElements)), nil
+}
+
+func (c Client) LRANGE(key string, start, stop int64) (elements []ReturnValue, err error) {
+	return
+}
+
+func (c Client) RPOP(key string) (element ReturnValue, err error) {
+	return
+}
+
+func (c Client) LPUSHX(key string, vElements ...interface{}) (newLength int64, err error) {
+	return
+}
+
+func (c Client) RPUSHX(key string, vElements ...interface{}) (newLength int64, err error) {
+	return
+}
+
+func (c Client) RPOPLPUSH(sourceKey string, destinationKey string) (element ReturnValue, err error) {
+	return
+}
+
+func (c Client) LSET(key string, index int64, element string) (ok bool, err error) {
+	return
+}
+
+// LREM removes the first occurrence on the given side of the given element.
+func (c Client) LREM(key string, side LSide, vElement interface{}) (newLength int64, done bool, err error) {
+	return
 }
